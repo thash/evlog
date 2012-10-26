1c01d567b34450181741379a92746cdcc9e97e3c
Hash <takuya21hashimoto@gmail.com>
2012-10-27 03:22:28 +0900

    relative file path

--- lib/evlog.rb
+++ lib/evlog.rb
@@ -4,7 +4,7 @@ require "rubygems"
 require "bundler/setup"
 Bundler.require
 
-$secret  = Hashie::Mash.new(YAML.load_file('./../secret.yml'))
+$secret  = Hashie::Mash.new(YAML.load_file(File.expand_path('../../secret.yml', __FILE__)))
 $leveldb = LevelDB::DB.new("/tmp/leveldb")
 
 ### Essense of EDAMTest.rb {{{
